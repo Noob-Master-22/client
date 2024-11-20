b438d470fd4f5d5c903c27c86422b2639c0a3561
Noob-Master-22 <nssr221b@gmail.com>
2024-11-20 21:10:27 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState, usestate} from "react";
+import React from 'react';
+import { Axios } from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [listofUsers,setlistOfUsers]= useState([
+     {age:25,userId:"Hehe",email:"hehe@gmail.com"}
+  ]
   );
+
+  useEffect(()=>{
+    Axios.get("https://localhost:3001/getUsers").then((response)=>{
+      setlistOfUsers(response.data)
+    })
+  },[]); 
+  return(
+
+  <div className="App">
+      <div className='UsersDisplay'>
+      {listofUsers.map((Users)=>{
+        return (<div>
+          <h1>Age: {Users.age}</h1>
+          <h1>UserId:{Users.UserId}</h1>
+          <h1>email:{Users.email}</h1>
+          </div>
+         );
+      })};
+      
+      </div>  
+    </div>
+  )
 }
 
 export default App;
